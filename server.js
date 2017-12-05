02c0bc2458599106849ba9a863720692d6073c99
Sara Sharp <sarareturns@gmail.com>
2017-12-05 09:14:05 -0500

    port change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
